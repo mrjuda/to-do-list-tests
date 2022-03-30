81ca933e9595e6b2445abed0007ad17bbb3e747c
judah <87juda@gmail.com>
2022-03-30 19:24:57 -0300

    Add basic structure to the delete-task

--- delete-task.js
+++ delete-task.js
@@ -1,16 +1,15 @@
 // delete-task.js
 
-const shelf = ["task1", "ttask2", "task3"];
+const shelf = ['First task', 'Second task', 'Third task'];
 console.log(shelf[0]);
-// removeBook = (id) => {
-//   for (let i = 0; i < this.shelf.length; i += 1) {
-//     if (this.shelf[i].id === parseInt(id, 10)) {
-//       this.shelf.splice(i, 1);
-//       this.updateShelf();
-//     }
-//     this.paintToPage();
-//     this.setCheckboxFieldListeners();
-//   }
-// }
 
-// module.exports = removeBook;
+removeBook = (id) => {
+  id -= 1;
+  console.log('shelf before: ' + shelf + '\n');
+  shelf.splice(id, 1);
+  return '\nshelf after: ' + shelf;
+}
+
+console.log(removeBook(3));
+
+module.exports = removeBook;
