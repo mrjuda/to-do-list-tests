b206cf7b7ae0029db8e9a67a7e1e165f8dd0138e
judah <87juda@gmail.com>
2022-03-30 18:53:28 -0300

    Add test for the 'add-task' function

--- add-task.js
+++ add-task.js
@@ -9,15 +9,21 @@ class Task {
 }
 
 newTask = (title) => {
-  this.bookshelf = new StrShelf();
+  // this.bookshelf = new StrShelf();
   const task = new Task();
   task.title = title;
   task.completed = false;
-  this.shelf.push(task);
-  this.updateShelf();
-  this.paintToPage();
-  this.setInputFieldListeners();
-  this.setCheckboxFieldListeners();
+  // this.shelf.push(task);
+  // this.updateShelf();
+  // this.paintToPage();
+  // this.setInputFieldListeners();
+  // this.setCheckboxFieldListeners();
+
+  if(title.length < 1) {
+    return 'empty';
+  } else {
+    return title.length;
+  }
 }
 
 module.exports = newTask;
--- add-task.test.js
+++ add-task.test.js
@@ -3,22 +3,16 @@
 const newTask = require('./add-task.js');
 
 const test1 = '';
-const test2 = '7 chars';
-const test3 = 'I have 16 chars';
+const test2 = 'My task has 20 chars';
 
 // test 1: less than 1 char
-test('String size is < 1', () => {
-  expect(strSize(test1)).toBe('<1 char');
+test('String size is empty', () => {
+  expect(newTask(test1)).toBe('empty');
 });
 
 // test 2: 7 chars
 test('String length is equal to 7', () => {
-  expect(strSize(test2)).toBe(7);
+  expect(newTask(test2)).toBe(20);
 });
 
-// test 3: more than 10 chars
-test('String length is > 10', () => {
-  expect(strSize(test3)).toBe('>10 chars');
-});
-
-console.log(strSize(test3));
+console.log(newTask(test2));
