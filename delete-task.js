7adc808a18c6c4ab26ecf540d8d609bcb1f27d87
judah <87juda@gmail.com>
2022-03-30 18:27:19 -0300

    Initial env testing

---
+++ delete-task.js
@@ -0,0 +1,6 @@
+// delete-task.js
+
+function sum(a, b) {
+  return a + b;
+}
+module.exports = sum;
---
+++ delete-task.test.js
@@ -0,0 +1,7 @@
+// delete-task.test.js
+
+const sum = require('./delete-task.js');
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(sum(1, 2)).toBe(3);
+});
