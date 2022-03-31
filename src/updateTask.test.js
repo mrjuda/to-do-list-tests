6fe8e62c9909843170b1d05f081cb25965a6b8ac
oshanedesign <oshane.design@gmail.com>
2022-03-31 16:37:56 -0500

    update task test 3

--- src/updateTask.test.js
+++ src/updateTask.test.js
@@ -3,6 +3,16 @@
 const updateTask = require('./updateTask.js');
 
 test('update item from the list', () => {
+    const taskArr = [
+      { id: 1, description: 'task-1', done: false },
+      { id: 2, description: 'task-2', done: false },
+      { id: 3, description: 'task-3', done: false },
+      { id: 4, description: 'task-4', done: false },
+    ];
+    expect(updateTask(taskArr, 0)).toBe(true);
+  });
+
+test('update item from the list', () => {
   const taskArr = [
     { id: 1, description: 'task-1', done: false },
     { id: 2, description: 'task-2', done: false },
@@ -11,3 +21,23 @@ test('update item from the list', () => {
   ];
   expect(updateTask(taskArr, 1)).toBe(true);
 });
+
+test('update item from the list', () => {
+    const taskArr = [
+      { id: 1, description: 'task-1', done: false },
+      { id: 2, description: 'task-2', done: false },
+      { id: 3, description: 'task-3', done: false },
+      { id: 4, description: 'task-4', done: false },
+    ];
+    expect(updateTask(taskArr, 2)).toBe(true);
+  });
+
+  test('update item from the list', () => {
+    const taskArr = [
+      { id: 1, description: 'task-1', done: false },
+      { id: 2, description: 'task-2', done: false },
+      { id: 3, description: 'task-3', done: false },
+      { id: 4, description: 'task-4', done: false },
+    ];
+    expect(updateTask(taskArr, 3)).toBe(true);
+  });
